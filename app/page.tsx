311ce63c8c340a2f03d3c6264653d3d642275a95
Luke Gelmi <luke.gelmi@jetcharge.com.au>
2025-05-22 20:22:37 +0800

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -337,7 +337,7 @@ export default function Home() {
                                     <div className="flex items-center gap-2">
                                         <MapPin className="h-5 w-5 text-emerald-500" />
                                         <span>
-                                            Available for in-person worshops in
+                                            Available for in-person workshops in
                                             the Perth metro area
                                         </span>
                                     </div>
